12d9e211a1feba20603b09726ffc079f2b8e6bb8
Curro <cucosunatt@gmail.com>
2023-03-11 01:53:46 +0100

    Mi primer ejercicio de js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 console.log("NO HACER NADA AÚN")
+
+document.getElementById('blue-button').addEventListener('click', cambiarColoresAzul);
+document.getElementById('green-button').addEventListener('click', cambiarColoresVerde);
+document.getElementById('reset-button').addEventListener('click', cambiarColoresOriginales);
+  
+  function cambiarColoresAzul() {
+    document.body.style.backgroundColor = 'blue';
+    document.body.style.color = 'lightblue';
+  }
+  
+  function cambiarColoresVerde() {
+    document.body.style.backgroundColor = 'green';
+    document.body.style.color = 'lightgreen';
+  }
+  
+  function cambiarColoresOriginales() {
+    document.body.style.backgroundColor = '#3d3d3d';
+    document.body.style.color = 'black';
+  }
